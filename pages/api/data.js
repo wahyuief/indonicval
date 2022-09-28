00193f42c7a9bd15d647b1ad4f0e4e5c60ffa91d
Wahyu <wahyuief@gmail.com>
2022-09-28 12:25:32 +0700

    indonicval

---
+++ pages/api/data.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).end()
+  if (req.headers.baby_yoda_token !== process.env.BABY_YODA_TOKEN) return res.status(401).end()
+
+  const { id } = req.query
+  if (!id) return res.status(400).json("ID parameter is required!").end()
+  if (!/^[0-9]+$/.test(id)) return res.status(400).json("Invalid parameter ID! The indonesian NIC is only number").end()
+  if (id.length !== 16) return res.status(400).json("Invalid parameter ID! The length of the Indonesian NIC is a 16 digit number").end()
+
+  const filePath = path.join(process.cwd(), 'data.json')
+  const jsonData = await fs.readFile(filePath, 'utf8')
+  const data = JSON.parse(jsonData)
+
+  const province = id.slice(0, 2)
+  const city = id.slice(0, 4)
+  const district = id.slice(0, 6)
+  const day = id.slice(6, 8)
+  const month = id.slice(8, 10)
+  const year = id.slice(10, 12)
+  const code = id.slice(12, 16)
+  const gender = day.length > 40 ? 'Female' : 'Male'
+
+  const details = {}
+  details['id'] = id
+  details['data'] = {}
+  details['data']['valid'] = true
+  details['data']['day'] = day
+  details['data']['month'] = new Date(year, month-1, day).toLocaleString('default', { month: 'long' })
+  details['data']['year'] = new Date(year, month, day).getFullYear()
+  details['data']['gender'] = gender
+  details['data']['district'] = capitalize(data['kecamatan'][district].split(' -- ')[0])
+  details['data']['city'] = capitalize(data['kabkot'][city])
+  details['data']['province'] = capitalize(data['provinsi'][province])
+  details['data']['postcode'] = data['kecamatan'][district].split(' -- ')[1]
+
+  res.status(200).json(details)
+}
+
+function capitalize(word) {
+  return word.toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+}
